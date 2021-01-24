c8bd609602a2cac77bbc6a0834a029ea21fde3ef
Abdullahi Bashiru B. <75587876+White-Kabar@users.noreply.github.com>
2021-01-23 23:21:01 -0800

    Update

---
+++ A/A.java
@@ -0,0 +1,19 @@
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double kn, thirdpow, y = 0;
+		kn = a(4);
+		thirdpow = cube(5);
+		System.out.println(kn);
+		System.out.println(thirdpow);
+	}
+	public static double a(int d) {
+		return 3*d +1;
+	}
+	public static double cube(double o) {
+		return Math.pow(5,  3);
+	}
+
+}
